5e477485c8058efb2670708dc518ad74a3fba770
Jimmy <montenegro.jimmy@gmail.com>
2026-09-10 00:55:54 -0300

    prueba de ruta

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return '¡Hola! La aplicación de facturas está funcionando correctamente en f.pfau.cl';
 });
